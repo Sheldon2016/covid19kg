eccfd9048001d03fb0bb7d499a250371c9f9878f
XD2016 <xiaodlee@foxmail.com>
2020-08-13 01:32:42 +0800

    Update Motif.java

--- codes/Motif.java
+++ codes/Motif.java
@@ -11,6 +11,29 @@ public class Motif {
 	public ArrayList<ArrayList<Integer>> motifLabelNodes = null;
 	public int edgeNum = 0;
 	
+	
+	void getTailedTriangle(){
+		motif = new ArrayList[4];
+		motif[0] = new ArrayList();//to store neighbors of 0
+		motif[0].add(1);
+		motif[1] = new ArrayList();
+		motif[1].add(0);
+		motif[1].add(2);
+		motif[1].add(3);
+		motif[2] = new ArrayList();
+		motif[2].add(1);
+		motif[2].add(3);
+		motif[3] = new ArrayList();
+		motif[3].add(1);
+		motif[3].add(2);
+		motifLabels = new ArrayList();
+		motifLabels.add(0);
+		motifLabels.add(1);
+		motifLabels.add(1);
+		motifLabels.add(3);
+		run();
+	}
+	
 	void getNode(){
 		motif = new ArrayList[1];
 		motif[0] = new ArrayList();//to store neighbors of 0
