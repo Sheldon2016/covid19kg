0284bc7fccce8ad98c2168b005efe0864d95e0bd
XD2016 <xiaodlee@foxmail.com>
2020-08-23 21:44:01 +0800

    Update MDISTest.java

--- codes/MDISTest.java
+++ codes/MDISTest.java
@@ -11,7 +11,7 @@ public class MDISTest {
 		MDIS a = new MDIS();
 		String labels = "A,B,C,D", kStr = 3 +"";
 		List<List<String>> res = a.mdis(labels, kStr);
-		System.out.println(res);
+		//System.out.println(res);
 	}
 
 }
