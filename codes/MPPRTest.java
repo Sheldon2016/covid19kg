c7d674668a191cd28430f68e3a6a6046dd66b31f
XD2016 <xiaodlee@foxmail.com>
2020-08-24 17:53:33 +0800

    new

---
+++ codes/MPPRTest.java
@@ -0,0 +1,19 @@
+package neo4jdriver;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import archive.MotifMatchOld;
+import datapre.covid19kg;
+import tools.Motif;
+
+public class MPPRTest {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		
+		MPPR m = new MPPR();
+		System.out.println(m.mppr("1122", "A,B,C,D", "5", "A", "A", 10000+"", 0.85+""));
+	}
+
+}
