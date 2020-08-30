56ad61cd747d8aebe6f85f165d3893e082fc53c3
Aryaman Dubey <joeydubey@gmail.com>
2020-08-31 00:51:46 +0800

    DrugID and frequency combined into HashMap

--- codes/neo4jdrivertest/MCountTest.java
+++ codes/neo4jdrivertest/MCountTest.java
@@ -2,8 +2,8 @@ package neo4jdrivertest;
 
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
 import java.util.Collections;
 
 import algo.MotifMatch;
@@ -15,27 +15,23 @@ public class MCountTest {
 		String nodes[] = {"Disease","HostProtein","Symptom","VirusProtein","Strain","Location"};
 		List<List<Integer>>motifIns = mc.mcount("3221", "HostProtein,Symptom,Drug,Virus");
 		//System.out.println(s);
-		ArrayList<Integer> drugs = new ArrayList<Integer>();
-		ArrayList<Integer> drugFreq = new ArrayList();
+		HashMap<Integer, Integer> drugs = new HashMap<Integer, Integer>();
 		for (int i = 0; i < motifIns.size(); i++) {
 			int drug = motifIns.get(i).get(2);
-			if (!drugs.contains(drug))
-				drugs.add(drug);
-			//drugs.add(motifIns.get(i).get(2));
-			drugFreq.add(0);
+			drugs.put(drug, 0);
 		}
-		for (int i = 0; i < drugs.size(); i++) {
-			int drug = drugs.get(i);
+		for (Integer i: drugs.keySet()) {
+			int drug = i;
 			for (int j = 0; j < motifIns.size(); j++) {
 				if (motifIns.get(j).contains(drug))
-					drugFreq.set(i, drugFreq.get(i)+1);
+					drugs.put(drug, drugs.get(i)+1);
 			}
 		}
-		
 		//System.out.println(motifIns);
 		System.out.println(motifIns.size());
-		for (int i = 0; i < drugs.size(); i++) {
-			System.out.println("DrugID: " + drugs.get(i) + ", Frequency: " + drugFreq.get(i));
+		
+		for (Integer i: drugs.keySet()) {
+			System.out.println("DrugID: " + i + ", Frequency: " + drugs.get(i));
 		}
 	}
 }
