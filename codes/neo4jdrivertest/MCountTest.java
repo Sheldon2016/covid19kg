d8a2a48ad56d2a28d86d07bc2105ad06e8e66428
Aryaman Dubey <joeydubey@gmail.com>
2020-08-31 00:35:32 +0800

    MCountTest updated for drug frequencies

--- codes/neo4jdrivertest/MCountTest.java
+++ codes/neo4jdrivertest/MCountTest.java
@@ -4,6 +4,7 @@ package neo4jdrivertest;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 import algo.MotifMatch;
 import neo4jdriver.MCount;
@@ -11,9 +12,30 @@ import neo4jdriver.MCount;
 public class MCountTest {
 	public static void main(String[]args) throws IOException {
 		MCount mc = new MCount();
+		String nodes[] = {"Disease","HostProtein","Symptom","VirusProtein","Strain","Location"};
+		List<List<Integer>>motifIns = mc.mcount("3221", "HostProtein,Symptom,Drug,Virus");
+		//System.out.println(s);
+		ArrayList<Integer> drugs = new ArrayList<Integer>();
+		ArrayList<Integer> drugFreq = new ArrayList();
+		for (int i = 0; i < motifIns.size(); i++) {
+			int drug = motifIns.get(i).get(2);
+			if (!drugs.contains(drug))
+				drugs.add(drug);
+			//drugs.add(motifIns.get(i).get(2));
+			drugFreq.add(0);
+		}
+		for (int i = 0; i < drugs.size(); i++) {
+			int drug = drugs.get(i);
+			for (int j = 0; j < motifIns.size(); j++) {
+				if (motifIns.get(j).contains(drug))
+					drugFreq.set(i, drugFreq.get(i)+1);
+			}
+		}
 		
-		List<List<Integer>>motifIns = mc.mcount("1122", "A,B,B,A");
-		
+		//System.out.println(motifIns);
 		System.out.println(motifIns.size());
+		for (int i = 0; i < drugs.size(); i++) {
+			System.out.println("DrugID: " + drugs.get(i) + ", Frequency: " + drugFreq.get(i));
+		}
 	}
 }
